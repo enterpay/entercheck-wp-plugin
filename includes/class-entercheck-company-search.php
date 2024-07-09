b5317186da454bfd4b4a466525871a7602c04d6b
ShimaAl <48447992+ShimaAl@users.noreply.github.com>
2024-07-09 15:37:24 +0200

    PLUG-30
    Transfer old options to new

--- includes/class-entercheck-company-search.php
+++ includes/class-entercheck-company-search.php
@@ -80,6 +80,7 @@ class Entercheck_Company_Search {
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
+		$this->update_options();
 		$this->define_options();
 	}
 
@@ -211,6 +212,32 @@ class Entercheck_Company_Search {
 		
 	}
 	
+	private function update_options(){
+		$options = get_option('enterpay_plugin_options');
+		if ($options && is_array($options) && count($options) > 0){
+			update_option('entercheck_plugin_options', $options, false);
+			delete_option('enterpay_plugin_options');
+		}
+		
+		$options = get_option('enterpay_plugin_options_fields');
+		if ($options && is_array($options) && count($options) > 0){
+			update_option('entercheck_plugin_options_fields', $options, false);
+			delete_option('enterpay_plugin_options_fields');
+		}
+		
+		$options = get_option('enterpay_plugin_options_form_mapping');
+		if ($options && is_array($options) && count($options) > 0){
+			update_option('entercheck_plugin_options_form_mapping', $options, false);
+			delete_option('enterpay_plugin_options_form_mapping');
+		}
+		
+		$enterpay_token = get_option('enterpay_token');
+		if ($enterpay_token && $enterpay_token != ''){
+			update_option('entercheck_token', $enterpay_token, false);
+			delete_option('enterpay_token');
+		}		
+	}
+	
 	private function define_options(){
 		$options_fields = get_option('entercheck_plugin_options_fields');		
 		if ($options_fields === false || !is_array($options_fields)){
@@ -218,10 +245,16 @@ class Entercheck_Company_Search {
 			update_option('entercheck_plugin_options_fields', $options_fields, false);
 		}
 		
-		$options = get_option('entercheck_plugin_options');		
+		$options = get_option('entercheck_plugin_options');
 		if ($options === false || !is_array($options)){
 			$options = [];
 			update_option('entercheck_plugin_options', $options, false);
+		}
+		
+		$options_form_mapping = get_option('entercheck_plugin_options_form_mapping');
+		if ($options_form_mapping === false || !is_array($options_form_mapping)){
+			$options_form_mapping = [];
+			update_option('entercheck_plugin_options_form_mapping', $options_form_mapping, false);
 		}
 	}
 
